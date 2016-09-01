7f9c0c770f32faec54c35e113989dd2e787ab807
tdvance <tdvance@gmail.com>
2016-09-01 10:55:54 -0400

    Add RigidBody2D and Collider2D to game pieces

---
+++ Assets/Menu/prefabs/Attackers/Attacker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour
+{
+
+    [Range(-1f, 1.5f)]
+    public float walkSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D myRigidBydoy = gameObject.AddComponent<Rigidbody2D>();
+        myRigidBydoy.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log(name + " trigger enter");
+    }
+}
