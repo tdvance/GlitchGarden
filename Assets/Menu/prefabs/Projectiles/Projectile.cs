86d45c43786df767ca9010486f4faf91ddb67f01
tdvance <tdvance@gmail.com>
2016-09-07 10:53:14 -0400

    Defender projectiles now can kill attackers
    also fix firing rate for gnome

--- Assets/Menu/prefabs/Projectiles/Projectile.cs
+++ Assets/Menu/prefabs/Projectiles/Projectile.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
     public float speed, damage;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);   
-	}
-    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject obj = collider.gameObject;
+        if (!obj.GetComponent<Attacker>())
+        {
+            return; //do no more if not an attacker
+        }
+        Debug.Log("Projectile " + name + " hit attacker " + obj);
+        obj.GetComponent<Health>().damage(damage);
+        Destroy(gameObject);
+    }
+
 }
